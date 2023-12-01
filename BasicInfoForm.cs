62c2de506767960f5d4e4e671b0ab0e6a0934a5e
Bohdan <duda.bohdan23@gmail.com>
2023-12-01 15:17:19 +0100

    BasicInfoForm validation + optimalized

--- BasicInfoForm.cs
+++ BasicInfoForm.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Text.RegularExpressions;
 
 namespace ServisniProtokol
 {
@@ -19,14 +11,30 @@ namespace ServisniProtokol
 
         private void btn_save_Click(object sender, EventArgs e)
         {
-            HomePageForm.instance.protocolNum.Text = this.txtBox_protocolNumber.Text;
-            HomePageForm.instance.date.Text = this.dateTimePicker.Value.ToString();
-            this.Close();
+            if (this.ValidateProtocolNum(this.txtBox_protocolNumber.Text))
+            {
+                HomePageForm.instance.protocolNum.Text = this.txtBox_protocolNumber.Text;
+                HomePageForm.instance.date.Text = this.dateTimePicker.Value.Date.ToString("dd.MM.yyyy");
+                this.Close();
+            }
         }
 
         private void btn_cancel_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private bool ValidateProtocolNum(string protocolNum)
+        {
+            bool isValid = false;
+            Regex regex = new("^\\d+$");
+            if (!regex.IsMatch(protocolNum))
+            {
+                this.errorProvider1.SetError(this.txtBox_protocolNumber, "Zadejte validní číslo protokolu!");
+                return isValid;
+            }
+
+            return isValid = true;
         }
     }
 }
